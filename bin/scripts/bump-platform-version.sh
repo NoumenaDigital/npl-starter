799183806a29087ffd33ddae92a5072ad54a52c2
pavlo-noumena <pavlo.skorodziievskyi@noumenadigital.com>
2024-06-03 10:00:09 +0200

    ST-4007

---
+++ bin/scripts/bump-platform-version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Usage: ./bump-platform-version.sh RELEASE_VERSION MAVEN_LOCAL_REPO
+
+RELEASE_VERSION=$1
+MAVEN_LOCAL_REPO=$2
+
+# Use '' after -i to make it work in CI(Linux) and on macOS locally
+sed -i '' 's|<platform.version>.*</platform.version>|<platform.version>'"${RELEASE_VERSION}"'</platform.version>|g' pom.xml
+mvn -Dmaven.repo.local=${MAVEN_LOCAL_REPO} $MAVEN_CLI_OPTS verify
